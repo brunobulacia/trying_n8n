c3613d510785c72a29c8db77054094738aa2bb9d
Bruno Bulacia <beueleceia@gmail.com>
2025-07-21 13:38:15 -0400

    carajoooo

--- tareas/serializers.py
+++ tareas/serializers.py
@@ -4,6 +4,6 @@ from .models import Tarea
 class TareaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tarea
-        fields = ['id', 'titulo', 'descripcion', 'completada', 'user_id']
+        fields = ['id', 'titulo', 'descripcion', 'completada']
         read_only_fields = ['id']
         
